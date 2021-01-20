669e570caef5b8c3f4ae1717106b410123e8adfd
TaeYoon17 <kim0517@hotmail.com>
2021-01-20 16:24:08 +0000

    specify add

--- req.py
+++ req.py
@@ -3,12 +3,14 @@ from save import save_to_file
 from Page import getPage
 def oneFile(url,num):
     def makeTextlist(value):
-        return value.text.strip()
+      spl=value.text.strip().split('\n')
+      spl[1]="("+spl[1]+")"
+      return "".join(spl)
     pageText=getPage(url)
     soup=BeautifulSoup(pageText,"html.parser")
     Sec=soup.find("section",{"class":"bg-lightgray section-sm"}).find("p").find("a").text
-    ParticipaintList=list(map(makeTextlist,soup.find("table",{"id":"datatable-PART_P_OC"}).find_all("td",{"data-title":"Country/Territory"})))
-    ObserveList=list(map(makeTextlist,soup.find("table",{"id":"datatable-PART_O_OC"}).find_all("td",{"data-title":"Country/Territory"})))
+    ParticipaintList=list(map(makeTextlist,soup.find("table",{"id":"datatable-PART_P_OC"}).find("tbody").find_all("tr")))
+    ObserveList=list(map(makeTextlist,soup.find("table",{"id":"datatable-PART_O_OC"}).find("tbody").find_all("tr")))
     pLen=len(ParticipaintList)
     oLen=len(ObserveList)
     lis=[]
@@ -18,7 +20,6 @@ def oneFile(url,num):
             ParticipaintList.append("")
         if i>=oLen:
             ObserveList.append("")
-        print(ParticipaintList[i],ObserveList[i])
         line=[Sec,ParticipaintList[i],ObserveList[i]]
         lis.append(line)
     save_to_file(lis,num)
